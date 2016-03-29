6d5a916b86a40115543e0be76929905cdad2cb39
alan-kinnek <alan@kinnek.com>
2016-03-29 13:41:43 -0400

    Added webpack

---
+++ edufai/edufai/static/js/imagepicker/ImagePickerApp.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+//var Router = require('react-router').Router;
+//var routes = require('./config/routes');
+/*
+ReactDOM.render(
+  <Router>{routes}</Router>,
+  document.getElementById('app')
+)
+*/
+
+ReactDOM.render(
+	<h1>Hello, world!</h1>,
+	document.getElementById('imagepicker')
+);
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+// In webpack.config.js
+var STATIC_URL = __dirname + '/edufai/edufai/static'
+
+module.exports = {
+	entry: [
+		STATIC_URL + '/js/imagepicker/ImagePickerApp.js',
+	],
+	output: {
+		path: STATIC_URL + '/js/dist',
+		filename: 'image_picker_bundle.js'
+	},
+	module: {
+		loaders: [
+			{test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
+		]
+	}
+};
